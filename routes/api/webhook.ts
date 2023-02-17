a575f1c6c25952647c715ac6550404c9a35b917b
drollinger <drollingerdallin@gmail.com>
2023-02-16 18:01:06 -0700

    changed to post

--- routes/api/webhook.ts
+++ routes/api/webhook.ts
@@ -1,6 +1,13 @@
-import { HandlerContext } from "$fresh/server.ts";
+import { HandlerContext, Handlers } from "$fresh/server.ts";
 
-export const handler = (req: Request, _ctx: HandlerContext): Response => {
-  console.log(req.body);
-  return new Response();
+export const handler: Handlers = {
+  POST(req: Request, _ctx: HandlerContext): Response {
+    console.log(req.body);
+    return new Response();
+  },
 };
+//export const handler: Handlers = {
+//  GET(_req: Request, _ctx: HandlerContext) {
+//    return new Response("Hello World");
+//  },
+//};
